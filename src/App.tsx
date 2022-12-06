801e8aaada69360a80027ae5e52d4f83c6d5cd23
Sarah Manicke <sm.wit2022@startsteps.de>
2022-12-06 15:29:39 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/* let name : string; name = "Sarah";*/
+/* let age : number; */
+/* let VarWithTwoDiffTypes : string | number; */
+/* let whatEver = any; das definiert einen any Typ */
+/* let isLoggedIn : boolean; */
+/* let hobbies : string[]; array of objects with strings */
+/* let role : [number, string]; can only contain one number and one string in exactly this order*/
+/* let person : object; einfachste Variante ein Objekt zu definieren */
+
+/* korrekte Variante ein Objekt zu definieren in TS 
+type Person = {
+  name: string;
+  age?: number; optionaler Eintrag/Property
+};
+
+let person : Person = {
+  name: "Sarah",
+  age: 33,
+};*/
+
+/* let LotsOfPeople : Person[]; So definiert man ein array mit Objekten */
+
+/* Wie man eine variable in einer Funktion deklariert
+let name : string;
+name = "Sarah";
+function showName(name : string){
+  console.log(name);
+}
+showName(name); */
+/* 
+let showMyName : Function; einfache deklarierung einer Funktion */
+
+/* let printName : (name:string) => void; korrekte deklarierung einer Funktion */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
